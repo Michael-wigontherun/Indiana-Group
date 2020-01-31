60f83ae513820889c385513d93807e61cf6701cc
Michael-wigontherun <43317302+Michael-wigontherun@users.noreply.github.com>
2020-01-31 14:39:36 -0500

    Update truckingDatabaseSQL.sql

--- Michael/truckingDatabaseSQL.sql
+++ Michael/truckingDatabaseSQL.sql
@@ -2,7 +2,7 @@ create database IndianaTruckingCompanys;
 go
 use IndianaTruckingCompanys;
 go
-create table LiquidatedCompanys(
+create table TruckingCompanies(--Trucking companies that are currently in oporation
 	USDOT varchar(6) primary key not null,
 	LegalNameDBAName varchar(200) not null,
 	Address varchar(500) not null,
@@ -13,4 +13,28 @@ create table LiquidatedCompanys(
 									--add when you push data into this from the website this field say
 									--update[today's date mm-dd-yyyy]
 );
-
+go
+create table ChameleonCompanies(--Trucking companies that are oporational but are expected to be chameleon companies
+	USDOT varchar(6) primary key not null,
+	LegalNameDBAName varchar(200) not null,
+	Address varchar(500) not null,
+	OOSReason varchar(41) not null,
+	OOSDate varchar(10) not null,
+	Status varchar(7) not null,
+	addminID nvarchar(450) not null --this is link going to be linked with the AspNetUsers dont 
+									--add when you push data into this from the website this field say
+									--update[today's date mm-dd-yyyy]
+	FOREIGN KEY (USDOT) REFERENCES TruckingCompanies(USDOT)
+);
+go
+create table LiquidatedCompanys(--No longer optorational companies
+	USDOT varchar(6) primary key not null,
+	LegalNameDBAName varchar(200) not null,
+	Address varchar(500) not null,
+	OOSReason varchar(41) not null,
+	OOSDate varchar(10) not null,
+	Status varchar(7) not null,
+	addminID nvarchar(450) not null --this is link going to be linked with the AspNetUsers dont 
+									--add when you push data into this from the website this field say
+									--update[today's date mm-dd-yyyy]
+);
