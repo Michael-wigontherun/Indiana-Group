d9a7953755198d6e558af29def85593f15d8ed24
Wig <Michael.ftt@outlook.com>
2020-02-04 11:24:05 -0500

    0.0.1

--- Michael/truckingDatabaseSQL.sql
+++ Michael/truckingDatabaseSQL.sql
@@ -55,7 +55,6 @@ create table ChameleonCompanies(--Trucking companies that are oporational but ar
 	addminID nvarchar(450) not null --this is link going to be linked with the AspNetUsers dont 
 									--add when you push data into this from the website this field say
 									--update[today's date mm-dd-yyyy]
-	FOREIGN KEY (USDOT) REFERENCES TruckingCompanies(USDOT)
 );
 go
 drop table LiquidatedCompanys
