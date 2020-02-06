9f0c99a1275719e9916a7b63090d34a6d3662254
Wig <Michael.ftt@outlook.com>
2020-02-06 10:01:12 -0500

    Update IndianaCSVconverter.java

--- Michael/IndianaCSVconverter/src/indianacsvconverter/IndianaCSVconverter.java
+++ Michael/IndianaCSVconverter/src/indianacsvconverter/IndianaCSVconverter.java
@@ -13,8 +13,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
